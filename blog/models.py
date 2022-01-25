5f1bba38c5d5c74cc22844a42a28c5e0245d5eaf
russel <russelcucse@gmail.com>
2022-01-25 13:16:01 +0600

    Blog detail and media url setup done

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
-class Blog(models.Model):
-    images = models.ImageField(upload_to='static/images/')
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+
+class Blog(models.Model):  
+    author = models.CharField(max_length=200, blank=True, null=True) 
+    title = models.CharField( max_length=200, blank=True, null=True)
+    images = models.ImageField(upload_to='images', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    period = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 def home(request):
-    return render(request, 'index.html') 
+    jobs = Blog.objects.all()
+    context = {'jobs': jobs}
+    return render(request, 'index.html', context)
+
+
+def detail(request, blog_id):
+    detailView = get_object_or_404(Blog, pk=blog_id)
+    context = {'blog': detailView }  
+    return render(request, 'detail.html', context) 
